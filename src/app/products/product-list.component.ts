94c71b0a1b63d7e95b1916ff25cb2c80fd102399
Vinicius Furukawa Carvalho <vinicius.furukawa@dcomp.sor.ufscar.br>
2020-04-17 11:52:37 -0300

    Ending of part 9

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { IProduct } from './products';
+import { ProductService } from './product.service';
 
 @Component({
     selector: 'pm-products',
@@ -11,8 +12,12 @@ export class ProductListComponent implements OnInit {
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-
     _listFilter: string = 'cart';
+    filteredProducts: IProduct[];
+    products: IProduct[];
+
+    constructor(private productService: ProductService) {
+    }
 
     get listFilter(): string {
         return this._listFilter;
@@ -23,41 +28,13 @@ export class ProductListComponent implements OnInit {
         this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
     }
 
-    filteredProducts: IProduct[];
-    products: IProduct[] = [
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2019",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "assets/images/garden_cart.png"
-        },
-        {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2019",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "assets/images/hammer.png"
-        }
-    ];
-
-    constructor() {
-        this.filteredProducts = this.products;
-        this.listFilter = 'cart';
-    }
-
     toggleImage(): void {
         this.showImage = !this.showImage;
     }
 
     ngOnInit(): void {
-        console.log('In OnInit');
+        this.products = this.productService.getProducts();
+        this.filteredProducts = this.products;
     }
 
     performFilter(filterBy: string): IProduct[] {
